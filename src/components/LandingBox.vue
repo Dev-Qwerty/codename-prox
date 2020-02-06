60ca8d5547c3d378506468336b12543717bf6fdc
alanmatw <alanmatw@gmail.com>
2020-02-07 03:25:53 +0530

    Create box component

---
+++ src/components/LandingBox.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class = "box">
+    <h3>YOUR SERVICE EXPERT</h3>
+    <h5>Get instant access to reliable</h5>
+    <h5>and affordable services</h5>
+    <div class="input-group mb-3">
+      <input type="text" class="form-control" placeholder="location" aria-label="Recipient's username" aria-describedby="basic-addon2">
+      <div class="input-group-append">
+        <button class="btn btn-outline-secondary" type="button">search</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>    
+  .box {
+    width: 600px;
+    height: 300px;
+    background-color: #fff;
+    opacity: 0.9;
+    top: 60%;
+    left: 30%;
+    position: absolute;
+    box-sizing: border-box;
+    transform: translate(-50%, -50%);
+    padding-top: 30px;
+    padding-bottom: 5px;
+    padding-left: 30px;
+    padding-right: 30px;
+    border-radius: 20px;
+  }
+
+  .box h3 {
+    color: #000;
+    padding: 0 0 20px;
+    text-align: center;
+    font-weight: bold;
+  }
+  .box h5 {
+    margin: 0;
+    color: #000;
+    padding: 0 0 20px;
+    text-align: center;
+  }
+</style>
