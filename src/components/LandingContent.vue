ce1df9ea2a02777e5efcbbf0b6658b9bf84861d2
alanmatw <alanmatw@gmail.com>
2020-02-07 03:19:48 +0530

    Create landing page - background image

---
+++ src/components/LandingContent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class = "landing"> 
+    <div class = "landing-content">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .landing {
+    min-height: 650px;
+    background-image: url('../assets/ManFactory.jpg');
+    background-blend-mode: overlay;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  }
+</style>
