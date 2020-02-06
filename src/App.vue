31f1d46dbe8b1b8a402438e8c398bbd43c20f7ba
alanmatw <alanmatw@gmail.com>
2020-02-07 03:15:30 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Main />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from './components/Main.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Main,
   }
 }
 </script>
@@ -19,10 +18,7 @@ export default {
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding-left: 100px;
+  padding-right: 100px;
 }
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Nav />
+    <Lcontent />  
+    <LandingBox />
+  </div>
+</template>
+
+<script>
+import Nav from '@/components/Navbar'
+import Lcontent from '@/components/LandingContent'
+import LandingBox from '@/components/LandingBox'
+
+export default {
+  components: {
+    Nav,
+    Lcontent,
+    LandingBox
+  }
+}
+</script>
+
+<style>
+</style>
