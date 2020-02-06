1341ee8aa303a68e4d103a366229f39432900422
alanmatw <alanmatw@gmail.com>
2020-02-07 03:17:32 +0530

    Create navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="navbar navbar-expand-sm bg-white navbar-light">
+    <!-- Brand/logo -->
+    <ul class="navbar-nav mr-auto">
+      <!-- <img src="../assets/logo.png" alt="Logo" style="width:40px;"> -->
+      <a class="navbar-brand" href="#">Urban Company</a>
+    </ul>
+
+    <!-- Links -->
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Get Hired</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Blog</a>
+      </li>
+      <button type="button" class="btn mr-1 btn-dark btn-sm">Login</button>
+      <button type="button" class="btn btn-outline-light text-dark btn-sm">SignUp</button>
+    </ul>
+  </nav>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
