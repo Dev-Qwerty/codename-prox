7f16eaa3cb42ceb355fe145ab7d1ec72dac79ce6
alanmatw <alanmatw@gmail.com>
2020-02-11 23:18:36 +0530

    Create login component

---
+++ client/src/components/Login/LMain.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Nav />
+    <Lwrapper />
+    <Lbox />
+    <LStackBox />
+  </div>
+</template>
+
+<script>
+import Nav from '@/components/Home/Navbar'
+import Lwrapper from '@/components/Login/Lwrapper'
+import Lbox from '@/components/Login/Lbox'
+import LStackBox from '@/components/Login/LStackBox'
+
+export default {
+  components: {
+    Nav,
+    Lwrapper,
+    Lbox,
+    LStackBox
+  }
+}
+</script>
---
+++ client/src/components/Login/LStackBox.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class = "box">
+    <h5>Get instant access to reliable</h5>
+    <h5>and affordable services</h5>
+  </div>
+</template>
+
+<style scoped>    
+  .box {
+    width: 450px;
+    height: 150px;
+    background-color: #000;
+    color: #fff;
+    opacity: 0.9;
+    top: 33%;
+    left: 50%;
+    position: absolute;
+    box-sizing: border-box;
+    transform: translate(-50%, -50%);
+    padding-top: 30px;
+    padding-bottom: 5px;
+    padding-left: 30px;
+    padding-right: 30px;
+    border-radius: 20px;
+  }
+
+  .box h3 {
+    color: #000;
+    padding-bottom: 6px;
+    text-align: center;
+    font-weight: bold;
+  }
+  .box h5 {
+    margin: 0;
+    color: #000;
+    padding-bottom: 0px; 
+    text-align: center;
+  }
+  .input-group {
+    padding-top: 40px;
+  }
+</style>
