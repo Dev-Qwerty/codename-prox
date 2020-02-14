f484fc126bfc9d220eeb3c87aab210e3c587498a
albinjose <albinavirappatt@gmail.com>
2020-02-14 07:32:58 +0530

    created subservices schema

---
+++ server/models/subservices.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+// Schema for subservices
+const subserviceSchema = new mongoose.Schema({
+    subserviceName: {
+        type: String,
+        required: true
+    },
+    preInspection: Number,
+    averageCost: {
+        type: Number,
+        required: true
+    },
+    averageTime: {
+        type: String,
+        type: required
+    },
+    comments: String
+})
