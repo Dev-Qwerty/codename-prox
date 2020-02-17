997bd6d42e9a56f37105c6e695eb98badc452403
alanmatw <alanmatw@gmail.com>
2020-02-17 20:47:28 +0530

    feat(GetHired): add image

--- client/src/components/GetHired/GHbox.vue
+++ client/src/components/GetHired/GHbox.vue
@@ -4,20 +4,30 @@
       <h1>GET A STEP FURTHUR</h1>
       <h1>JOIN US.</h1>
     </div>
+    <div class="workersimg">
+    </div>  
   </div>  
 </template>
 
 <style scoped>
-  .wrapper {
+  .box {
     padding: 100px;
     width: 100%;
     height:300px;
     background-color: #eeefef;
   }
-  .wrapper h1 {
+  .box h1 {
     margin: 0px;
     padding: 0px;
     font-family: 'Times New Roman';
     font-size: 45px;
+  }
+  .workersimg {
+    height: 350px;
+    width: 100%;
+    background-image: url('../../assets/Workers.png');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: contain;
   }
 </style>
