93ec498f7bba2fc8d675dd6f38e1268dae489804
alanmatw <alanmatw@gmail.com>
2020-02-17 21:32:52 +0530

    feat(GetHired): add new section

--- client/src/components/GetHired/GHbox.vue
+++ client/src/components/GetHired/GHbox.vue
@@ -1,11 +1,25 @@
 <template>
-  <div class="wrapper">
+  <div class="conatiner">
     <div class="box">
       <h1>GET A STEP FURTHUR</h1>
       <h1>JOIN US.</h1>
     </div>
     <div class="workersimg">
-    </div>  
+    </div>
+    <div class="wrapper">
+      <div class="textarea">
+        <div class="heading">
+          <h2>We've made a platform for you to</h2>
+          <h2>connect with customers</h2> 
+        </div>
+        <p>Urban Company provides you an interface to meet with</p>
+        <p>your customer to chat and make a deal to get their</p>
+        <p>work done. So why waiting..</p>
+        <input type="submit" value="GET HIRED">
+      </div>
+      <div class="img">
+      </div>
+    </div>
   </div>  
 </template>
 
@@ -30,4 +44,45 @@
     background-position: center;
     background-size: contain;
   }
+  .wrapper {
+    margin-left: 50px;
+    margin-right: 50px;
+    margin-top: 100px;
+    margin-bottom: 100px;
+    display: flex;
+  }
+  .textarea h2 {
+    margin: 0px;
+    font-size: 35px;
+    font-style: bold;
+  }
+  .textarea p {
+    margin: 0px;
+    font-size: 18px;
+  }
+  .textarea input[type="submit"] {
+    border: none;
+    background-color: #4d4d4f;
+    color: #fff;
+    margin-top: 30px;
+    width: 210px;
+    height: 50px;
+    font-family: Arial;
+    font-size: 22px;
+  }    
+  .heading {
+    margin-bottom: 20px;
+  }
+  .img {
+    height: 300px;
+    flex-grow:1;
+    overflow: hidden;
+    perspective: 1px;
+    background-image: url('../../assets/paint.png');
+    background-blend-mode: overlay;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: contain;    
+  }  
+
 </style>
