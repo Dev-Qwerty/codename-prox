254d6e3a234914474bf4e1bb4bc2e15aecbd7755
Amal Jose <amalavirappatt@gmail.com>
2020-02-20 23:57:07 +0530

    controller for updating user profile'

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const config = require('./config/mongo-connect');
 const passport = require('passport')
 const passportSetup = require('./config/passport-config')
@@ -8,9 +9,12 @@ const workerRouter = require('./routes/worker')
 
 const app = express();
 
+
 app.set('view engine','ejs')
 app.use(passport.initialize())
 app.use(passport.session())
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:false}))
 
 
 app.use('/auth', authRouter)
@@ -20,6 +24,10 @@ app.use('/worker', workerRouter)
 // Home Route
 app.get('/', (req,res) => {
     res.render('home')
+})
+
+app.get('/gethired', (req, res) => {
+    res.send({message: "ok"})
 })
 
 
