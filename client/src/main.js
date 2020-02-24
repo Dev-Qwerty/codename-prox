9725c64dcd42f74442e67ec0a3f52a931155a2d9
alanmatw <alanmatw@gmail.com>
2020-02-24 21:45:54 +0530

    feat: configure routes

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Home from './components/Home/Main.vue'
+import GetHired from './components/GetHired/GHMain.vue'
+import Signup from './components/Signup/SMain.vue'
+import Login from './components/Login/LMain.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const routes = [
+  {path: '/', component: Home },
+  {path: '/gethired', component: GetHired },
+  {path: '/signup', component: Signup },
+  {path: '/login', component: Login }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
