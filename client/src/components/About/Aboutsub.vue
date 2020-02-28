53b81533b5eb08d7fcc33640bbe5707071ce8fe9
christy19 <christyoomervarghese2021@cs.ajce.in>
2020-02-28 16:34:42 +0530

    feat(About): create initial structure

---
+++ client/src/components/About/Aboutsub.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="conatiner">
+    <div class="box">
+        <h1>URBAN COMPANY</h1>
+        <h2>One stop destination for services</h2>
+    </div>
+    <div class="qw">
+        <p><b>Urban company is a startup based company which helps people to get their services done by expert professionals on time at affordable price at your home.</b></p>
+    </div>    
+  </div>      
+</template>
+
+<style scoped>
+.box{
+    width: 100%;
+    padding: 100px;
+    height: 300px;
+    background-color: #eeefef;
+}
+.box h1 {
+    margin: 0px;
+    padding: 0px;
+    font-family: 'Times New Roman';
+    font-size: 60px;
+}
+.box h2{
+    margin: 0px;
+    padding: 0px;
+    font-family: 'Times New Roman';
+    font-size: 40px;
+}
+.qw {
+    margin: 0px;
+    padding-top: 50px;
+    padding-left: 0px;
+}
+.qw p {
+    font-size: 20px;
+}
+</style>
