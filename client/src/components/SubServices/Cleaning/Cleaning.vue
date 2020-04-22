8b774e02c66363265c33aecd5d73c0aba036a6e0
alanmatw <alanmatw@gmail.com>
2020-04-22 17:51:26 +0530

    feat: create directory hierarchy for subservices

---
+++ client/src/components/SubServices/Cleaning/Cleaning.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="Container">
+    
+        
+  </div>    
+</template>
+
+<style scoped>
+  
+</style>
---
+++ client/src/components/SubServices/Cleaning/Cmain.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Swrapper />
+  </div>
+</template>
+
+<script>
+import Cleaning from '@/components/Services/SubServices/Cleaning'
+
+export default {
+  components: {
+    Cleaning
+  }
+}
+</script>
