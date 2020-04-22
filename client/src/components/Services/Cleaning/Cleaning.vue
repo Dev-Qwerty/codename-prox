15d6a89a1d2ae9381c83b07ed2f41fb6822770de
alanmatw <alanmatw@gmail.com>
2020-04-22 19:12:30 +0530

    feat: workarounds

--- client/src/components/Services/Cleaning/Cleaning.vue
+++ client/src/components/Services/Cleaning/Cleaning.vue
@@ -3,11 +3,15 @@
     <div class="top-box">
       <h3>Back to services</h3>
     </div>
-    <div class="mid-box">
-      <h1>Quality Services</h1>
-      <h1>Served wiht Hygeine.</h1> 
-      <img src="" alt="">
+    <div class="mid">
+      <div class="mid-box">
+        <h1>Quality Services</h1>
+        <h1>Served with Hygeine.</h1> 
+        <div class="Line"></div>
+      </div>
+      <div class="mid-img"></div>
     </div>
+
   </div>    
 </template>
 
@@ -25,20 +29,39 @@
     font-style: italic;
     font-size: 21px;
   }
+  .mid {
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    background-color: #eeefef;
+    width: 100%;
+    height:350px;
+  }
   .mid-box {
-    box-sizing: border-box;
     padding-top: 100px;
     padding-bottom: 100px;
     padding-left: 40px;
-    width: 100%;
-    height:350px;
-    background-color: #eeefef;
   } 
   .mid-box h1 {
     margin: 0px;
     padding: 0px;
     font-size: 60px;
     color: #000;
+    font-weight: bold;
   }  
+  .Line {
+    margin-top: 10px;
+    width: 250px;
+    height: 5px;
+    background-color: #000;
+  }
+  .mid-img {
+    margin-top: 5px;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 340px;
+    width: 340px;
+    background-image: url(../../../assets/mbox.png);    
+  }
 
 </style>
