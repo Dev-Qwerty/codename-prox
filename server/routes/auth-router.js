1e2a19cf0247066a7cefe40cf1168b7a4f1fc768
James Kaviyil Jose <jamesjose03@gmail.com>
2020-04-23 10:10:48 +0530

    feat: basic signup backend implemented

--- server/routes/auth-router.js
+++ server/routes/auth-router.js
@@ -1,21 +1,48 @@
-const router = require('express').Router()
-const passport = require('passport')
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const User = require('../models/user-model');
 
-router.get('/login', (req,res) => {
-    res.render('login')
-})
+router.post('/register', (req, res) => {
+    const { email, phone, password } = req.body;
+    let errors = [];
+  
+    if (!email || !phone || !password) {
+      errors.push({ msg: 'Please enter all fields' });
+    }
+    if (password.length < 6) {
+      errors.push({ msg: 'Password must be at least 6 characters' });
+    }
+  
+    if (errors.length > 0) {
+      res.send({"status": "error", "errors": errors})
+    } else {
+      User.findOne({ email: email }).then(user => {
+        if (user) {
+          errors.push({ msg: 'Email already exists' });
+          res.send({"status": "error", "errors": errors})
+        } else {
+          const newUser = new User({
+            email,
+            phone,
+            password
+          });
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if (err) throw err;
+              newUser.password = hash;
+              newUser
+                .save()
+                .then(user => {
+                  res.send({"status": "Success", "user": user.name, "email": user.email, "category": user.category })
+                })
+                .catch(err => console.log(err));
+            });
+          });
+        }
+    });
+    }
+});
 
-router.get('/logout', (req,res) => {
-    //TO DO
-    res.send("Logging out")
-})
-
-router.get('/google', passport.authenticate('google', {
-    scope: ['profile']
-}))
-
-router.get('/google/redirect', passport.authenticate('google') ,(req,res) => {
-    res.redirect('/profile')
-})
-
-module.exports = router
+module.exports = router;
