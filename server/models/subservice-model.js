d3ac01f3b6ad5a47af08c8568f7a94af961051ee
albinjose <albinavirappatt@gmail.com>
2020-04-26 11:16:36 +0530

    update subservices schema

--- server/models/subservice-model.js
+++ server/models/subservice-model.js
@@ -1,22 +1,30 @@
 const mongoose = require('mongoose');
 
 const subserviceSchema = mongoose.Schema({
-    name: {
+    mainserviceId: {
         type: String,
         required: true
     },
-    description: {
-        type: String,
-        required: true
-    },
-    imagePath: {
-        type: String,
-        required: true
+    subservices: {
+        name: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        imagePath: {
+            type: String,
+            required: true
+        }
+
     },
     moreDetail: {
         type: String,
         required: true
     }
+
 })
 
 const subservice = mongoose.model('subservices', subserviceSchema);
---
+++ server/routes/subservice-router.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const router = express.Router();
+
+let subserviceModel = require('../models/subservice-model');
