54980d2c161382bd6336898287081e1ade591cc1
alanmatw <alanmatw@gmail.com>
2020-05-15 00:14:41 +0530

    feat(workreq): divide into two main sections

--- client/src/components/Dashboard/workreq.vue
+++ client/src/components/Dashboard/workreq.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="wr-wrapper">
-    <div class="wr-grid"></div>
+    <div class="wr-grid">
+      <div class="incomming-req"></div>
+      <div class="your-schedule"></div>
+    </div>
   </div>    
 </template>
 
@@ -12,9 +15,15 @@ export default {
 
 <style scoped>
   .wr-grid {
-    width: 50%;
-    margin-left: 50px;
-    height: 100px;
+    width: 100%;
+    height: 676px;
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+  }
+  .incomming-req {
     background-color: red;
+  }
+  .your-schedule {
+    background-color: aqua;
   }
 </style>
