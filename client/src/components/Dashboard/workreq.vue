474f931d24cc7cfabaa132f5bd1ed7f1b7ceb64c
alanmatw <alanmatw@gmail.com>
2020-05-15 00:34:04 +0530

    feat(workreq): create incomming req box grid structure

--- client/src/components/Dashboard/workreq.vue
+++ client/src/components/Dashboard/workreq.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="wr-wrapper">
     <div class="wr-grid">
-      <div class="incomming-req"></div>
+      <div class="incomming-req">
+        <p class="ir-header">Incomming Requests</p>
+        <div class="ir-box">
+          <div class="ir-box-one"></div>
+          <div class="ir-box-two"></div>
+          <div class="ir-box-three"></div>
+          <div class="ir-box-four"></div>
+        </div>
+      </div>
       <div class="your-schedule"></div>
     </div>
   </div>    
@@ -21,9 +29,33 @@ export default {
     grid-template-columns: 2fr 1fr;
   }
   .incomming-req {
-    background-color: red;
+    padding-left: 50px;
+  }
+  .ir-header {
+    margin-bottom: 5px;
+    font-size: 15px;
+    color: #000;
+  }
+  .ir-box {
+    width: 95%;
+    background-color: #fff;
+    height: 70px;
+    display: grid;
+    grid-template-columns: 55% 25% 10% 10%;
+  }
+  .ir-box-one {
+    border: 1px solid #aaa
+  }
+  .ir-box-two {
+    border: 1px solid #aaa
+  }
+  .ir-box-three {
+    border: 1px solid #aaa
+  }
+  .ir-box-four {
+    border: 1px solid #aaa
   }
   .your-schedule {
-    background-color: aqua;
+    border: 1px solid #aaa
   }
 </style>
