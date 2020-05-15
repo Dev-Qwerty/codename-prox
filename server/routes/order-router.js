bde697e4915f9866707cdf8129f5a685864f5e2b
albinjose <albinavirappatt@gmail.com>
2020-05-15 09:26:31 +0530

    refactor: add name of services instead of id

--- server/routes/order-router.js
+++ server/routes/order-router.js
@@ -11,32 +11,36 @@ router
     .post(async (req, res) => {
         try {
             global.totalAmount = 0;  // total amount variable
-            // calculate total amount
+            global.newOrder = new orderModel;  // create new order instance
+
+            // calculate total amount and category
             for (i = 0; i < req.body.service.categories.length; i++) {
-                try {
-                    const amount = await subserviceModel.find({ categories: { $elemMatch: { _id: req.body.service.categories[i].categoryId } } }, { 'categories.$': "5ead369bd1662c51584b7dd5" })
-                    itemAmount = amount[0].categories[0].amount * req.body.service.categories[i].quantity;
-                    totalAmount = totalAmount + itemAmount;
-                } catch (error) {
-                    res.json({ "message": "Failed to place Order" })
-                }
+                const serviceDetails = await subserviceModel.find({ categories: { $elemMatch: { _id: req.body.service.categories[i].categoryId } } }, { 'categories.$': "5ead369bd1662c51584b7dd5" })
+                itemAmount = serviceDetails[0].categories[0].amount * req.body.service.categories[i].quantity;
+                totalAmount = totalAmount + itemAmount;
+                req.body.service.categories[i].categoryId = serviceDetails[0].categories[0].category  // saving category name insted of categoryId
             }
 
+            // Find name of the subservice
+            const id = req.body.service.serviceId
+            const service = await subserviceModel.findById(id)
+
+
             // create new orderrs
-            let newOrder = new orderModel;
             newOrder.orderId = 379884;
             newOrder.userId = "adjfne3";
             newOrder.date = req.body.date;
-            newOrder.service.subserviceId = req.body.service.serviceId;
+            newOrder.service.subserviceId = service.name;
             newOrder.service.categories = req.body.service.categories;
             newOrder.totalAmount = totalAmount;
             newOrder.paid = false;
             newOrder.address = req.body.address;
 
-            // newOrder.save()
+            newOrder.save()
             res.json({ "message": "Order placed succesfully" })
 
         } catch (error) {
+            console.log(error)
             res.json({ "message": "Failed to place Order" })
         }
 
