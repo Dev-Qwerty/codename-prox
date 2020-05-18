9f9ce75c3a168f90f96374e9d35d10a7846286be
alanmatw <alanmatw@gmail.com>
2020-05-18 20:45:43 +0530

    feat(myworks): create box

--- client/src/components/Dashboard/myworks.vue
+++ client/src/components/Dashboard/myworks.vue
@@ -4,7 +4,20 @@
       <p class="ty-header">Today</p>
       <div class="box-wrapper">
         <div class="box sh-all">
-
+          <div class="box-row1">
+            <p class="row1-one">$99</p>
+            <div class="Line"></div>
+          </div>
+          <div class="box-row2">
+            <p class="row2-one">Kitchen Cleaning</p>
+            <p class="row2-two">Kanjirappally, Kottayam</p>
+          </div>
+          <div class="box-row3">
+            <p class="row3-one">8:30 AM</p>
+          </div>
+          <div class="box-row4">
+            <p class="row4-one">Work Details</p>
+          </div>
         </div>
       </div>
     </div>
@@ -35,10 +48,53 @@ export default {
     width: 25%;
     height: 200px;
     display: grid;
+    grid-template-rows: 30% 25% 30% 15%;
     background-color: #fff;
     border: 1px solid #F5F5F5;    
   } 
   .sh-all {
     box-shadow: 0 0 5px #DBDBDB;
   }    
+  .box-row1 {
+    text-align: center;
+  }
+  .row1-one {
+    text-align: center;
+    font-size: 40px;
+    font-weight: bold;
+    margin: 0px;
+  }
+  .Line {
+    margin-top: -5px;
+    margin-left: 70px;
+    height: 1px;
+    width: 120px;
+    background-color: #aaa;
+  }
+  .box-row2 {
+    text-align: center;
+  }
+  .row2-one {
+    font-size: 22px;
+    font-weight: bold;
+    margin: 0px;
+  }
+  .row2-two {
+    margin-top: -6px;
+    font-size: 14px;
+    color: #aaa;
+  }
+  .box-row3 {
+    text-align: center;
+    padding-top: 5px;
+  }
+  .row3-one {
+    font-size: 40px;
+  }
+  .box-row4 {
+    text-align: center
+  }
+  .row4-one {
+    font-size: 14px;
+  }
 </style>
