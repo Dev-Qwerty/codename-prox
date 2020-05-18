160a071d108ade5a991129f8c0715334616cc2ee
alanmatw <alanmatw@gmail.com>
2020-05-18 20:24:05 +0530

    feat(myworks): create box structure

--- client/src/components/Dashboard/myworks.vue
+++ client/src/components/Dashboard/myworks.vue
@@ -1,5 +1,14 @@
 <template>
-    
+  <div class="mw-wrapper">
+    <div class="mw-body">
+      <p class="ty-header">Today</p>
+      <div class="box-wrapper">
+        <div class="box sh-all">
+
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +18,27 @@ export default {
 </script>
 
 <style scoped>
-
+  .mw-body {
+    padding-top: 10px;
+    padding-left: 30px;
+  }
+  .ty-header {
+    font-weight: bold;
+    margin-bottom: -5px;
+    font-size: 15px;
+    color: #000;    
+  }
+  .box-wrapper {
+    margin-top: 10px;
+  }
+  .box {
+    width: 25%;
+    height: 200px;
+    display: grid;
+    background-color: #fff;
+    border: 1px solid #F5F5F5;    
+  } 
+  .sh-all {
+    box-shadow: 0 0 5px #DBDBDB;
+  }    
 </style>
