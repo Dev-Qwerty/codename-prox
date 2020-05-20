98cb19c10d59241634f1c9ecdd883cb40d17fc66
alanmatw <alanmatw@gmail.com>
2020-05-20 19:42:41 +0530

    feat(cart): create checkout component

---
+++ client/src/components/Services/Cleaning/checkout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="checkout-wrapper">
+    <div>
+      
+    </div>
+  </div>    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
