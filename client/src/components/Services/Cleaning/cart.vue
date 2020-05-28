c167c163fa196a7a3c0b753eb89460b0a71857ae
alanmatw <alanmatw@gmail.com>
2020-05-28 18:04:47 +0530

    feat(cart): create grid structure for checkout box

--- client/src/components/Services/Cleaning/cart.vue
+++ client/src/components/Services/Cleaning/cart.vue
@@ -1,21 +1,41 @@
 <template>
   <div class="cart-wrapper">
     <div class="cart">
-      <div class="top-box">
+      <div class="top-row">
         <router-link class="arrowIcon" :to="{ path: '/services/cleaning/modal' }"></router-link>
       </div>
       <div class="cart-body">
-        <div class="box1">
+        <div class="col1">
           <p>Shopping Cart</p>
         </div>
-        <div class="box2 sh-all">
-          <div class="b2-1">
-            <p>ORDER SUMMARY</p>
+        <div class="col2 sh-all">
+          <div class="col2-row1">
+            <p class="col2-row1-p">ORDER SUMMARY</p>
           </div>
           <div class="Line"></div>
-          <div class="b2-2"></div>
+          <div class="col2-row2">
+            <div v-for="n in 3" v-bind:key="n">
+              <div class="items-wrapper">
+                <div class="col2-row2-col1">
+                  <p class="col2-row2-col1-p">items 1</p>
+                </div>
+                <div class="col2-row2-col2">
+                  <p class="col2-row2-col2-p">$299.00</p>
+                </div>
+              </div>
+            </div>
+          </div>
           <div class="Line"></div>
-          <div class="b2-3"></div>
+          <div class="col2-row3">
+            <div class="items-wrapper">
+              <div class="col2-row3-col1">
+                <p class="col2-row3-col1-p">items 1</p>
+              </div>
+              <div class="col2-row3-col2">
+                <p class="col2-row3-col2-p">$299.00</p>
+              </div>
+            </div>            
+          </div>
         </div>
       </div>
     </div>
@@ -34,10 +54,10 @@ export default {
     top: 0;
     left: 0;
     width: 100%;
-    height: 50%;
+    height: 100%;
     background-color: #fff;
   }
-  .top-box {
+  .top-row {
     height: 50px;
     background-color: #000;
     width: 100%; 
@@ -62,11 +82,11 @@ export default {
     display: grid;
     grid-template-columns: 3fr 1fr;
   }
-  .box1 {
+  .col1 {
     height: 200px;
     /*background-color: aliceblue;*/
   }
-  .box2 {
+  .col2 {
     height: 400px;
     background-color: #f2f2f2;
     display: grid;
@@ -75,18 +95,37 @@ export default {
   .sh-all {
     box-shadow: 0 0 3px #d9d9d9;
   } 
-  .b2-1 {
- 
+  .col2-row1 {
+    padding-left: 15px;
+    padding-top: 26px;
+  }
+  .col2-row1-p {
+    font-size: 17px;
+    font-weight: bold;
   }
   .Line {
     margin-left: 15px;
     width: 90%;
     background-color: #cecece;
   }
-  .b2-2 {
-
+  .col2-row2 {
+    padding-top: 10px;
+    padding-left: 15px;
   }
-  .b2-3 {
+  .items-wrapper {
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    margin-bottom: 10px;    
+  }
+  .col2-row2-col1-p {
+    font-size: 16;
+    margin: 0px;
+  }
+  .col2-row2-col2-p {
+    font-size: 16;
+    margin: 0px;
+  }  
+  .col2-row3 {
 
   }
 </style>
