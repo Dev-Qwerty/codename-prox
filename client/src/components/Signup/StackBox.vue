cecd2e2308c9c9b261954bd76b2bd16ddbc7be0c
alanmatw <alanmatw@gmail.com>
2020-05-31 21:38:54 +0530

    refactor(signup): restructure signup page

---
+++ client/src/components/Signup/StackBox.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class = "box">
+    <h4>SignUp as</h4>
+  </div>
+</template>
+
+<style scoped>    
+  .box {
+    width: 27%;
+    height: 165px;
+    background-color: #000;
+    color: #fff;
+    opacity: 0.9;
+    top: 33%;
+    left: 50%;
+    position: absolute;
+    box-sizing: border-box;
+    transform: translate(-50%, -50%);
+    padding: 0px;
+    border-radius: 10px;
+    padding: 20px;
+    border: 1px solid #000;
+    box-shadow: 7px 7px #eeefef;
+  }
+  .box h4 {
+    font-size: 30px;
+    color: #fff;
+    margin-left: 20px;
+    margin-top: 45px;
+  }
+</style>
