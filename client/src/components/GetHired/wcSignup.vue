459cb7787dae3dc8f7e2d1293755751f7ca40370
alanmatw <alanmatw@gmail.com>
2020-06-10 18:53:48 +0530

    feat(wcsignup): create main grid

--- client/src/components/GetHired/wcSignup.vue
+++ client/src/components/GetHired/wcSignup.vue
@@ -1,8 +1,15 @@
 <template>
-  <div class="Modal">
+  <div class="wcsignup">
     <div class="Mbg">
-      <div class="Mbox">
-        <h1>hello</h1>        
+      <div class="sh-left">
+        <div class="Mbox sh-bottom">
+          <div class="leftPic">
+          
+          </div>
+          <div class="right">
+            
+          </div>      
+        </div>
       </div>
     </div>
   </div>  
@@ -33,11 +40,29 @@ export default {
     width:100%;
     height: 722px;
     padding-top: 3%;
-    padding-left: 15%;
+    padding-left: 14%;
   }
   .Mbox {
-    width: 80%;
+    width: 85%;
     height: 610px;
-    background-color: #aaa;
+    display: grid;
+    grid-template-columns: 60% 40%;
+  }  
+  .sh-bottom {
+    box-shadow: 0 5px 5px -6px #707070;
+  }
+  .sh-left {
+    box-shadow: -5px 0 5px -6px #707070;
   }    
+  .leftPic {
+    margin: 0px;
+    width: 100%;
+    background-image: url(../../assets/wcsignup.png);
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;  
+  }
+  .right {
+    background-color: #fff;
+  }
 </style>
