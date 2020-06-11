9d46956b7a967400a94d6b0216a2a9b25a7b9b4b
James Kaviyil Jose <jamesjose03@gmail.com>
2020-06-11 21:19:56 +0530

    feat: add completedProfile attribute to schemas

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -49,6 +49,10 @@ const UserSchema = new mongoose.Schema({
     name: {
         type: String,
         default: ""
+    },
+    completedProfile: {
+        type: Boolean,
+        default: false
     }
 });
 
