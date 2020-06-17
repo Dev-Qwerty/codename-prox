0e831f6b200f8cd46517a970851fcbab14fb1c3c
alanmatw <alanmatw@gmail.com>
2020-06-17 20:13:24 +0530

    fix: minor fix

--- server/routes/workrequest-route.js
+++ server/routes/workrequest-route.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const moment = require('moment')
 const router = express.Router()
-const sendMessage = require('../misc/textmessage')
+//const sendMessage = require('../misc/textmessage')
 
 // Import Models
 const workerRequest = require('../models/workrequest-model')
@@ -49,7 +49,7 @@ router
                         orderID: orderId
                     }
                     console.log(workDetails)
-                    sendMessage.sendTextMessage("customer",customerDetails,workDetails);
+                    //sendMessage.sendTextMessage("customer",customerDetails,workDetails);
                 } else {
                     let selectedWorkersDetails = await selectedWorkers.findOne({orderID: orderId}, '-_id')
                     let declinedWorker = selectedWorkersDetails.selectedWorkers.shift() //Return first worker
@@ -102,7 +102,7 @@ router
                         date: work.date,
                         time: work.time,
                     }
-                    sendMessage.sendTextMessage("worker",workerDetails,workDetails);
+                    //sendMessage.sendTextMessage("worker",workerDetails,workDetails);
                 }          
             }
         } catch (error) {
