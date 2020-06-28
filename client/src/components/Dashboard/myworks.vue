f654119477f7d81c43dbaf64bd6479d7f32186e4
alanmatw <alanmatw@gmail.com>
2020-06-28 13:22:09 +0530

    feat(myworks): add dependency for popover

--- client/src/components/Dashboard/myworks.vue
+++ client/src/components/Dashboard/myworks.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mw-wrapper">
     <div class="mw-body">
-      <p class="ty-header">Today's Work</p>
+      <p class="ty-header">Today's Work</p>    
       <div class="box-wrapper">
         <div v-for="(sr, index) in myworksarr.todaysWork" :key="index">
           <div class="box sh-all">
@@ -26,7 +26,7 @@
       <p class="yd-header">Upcomming Works</p>
       <div class="box-wrapper">
         <div v-for="(sr, index) in myworksarr.upcommingWorks" :key="index">
-          <div class="box sh-all">
+          <!--<div class="box sh-all">
             <div class="box-row1">
               <p class="row1-one"><span>$</span>{{ sr.totalAmount }}</p>
               <div class="Line"></div>
@@ -37,6 +37,41 @@
             <div class="box-row3">
               <p class="row3-one">{{ sr.date }}</p>
             </div>            
+          </div>-->          
+          <div class="box sh-all">
+            <div class="box-row1">
+              <p class="row1-one">$99</p>
+              <div class="Line"></div>
+            </div>
+            <div class="box-row2">
+              <p class="row2-one">Kitchen Cleaning</p>
+              <p class="row2-two">Kanjirappally, Kottayam</p>
+            </div>
+            <div class="box-row3">
+              <p class="row3-one">8:30 AM</p>
+            </div>
+            <div class="box-row4">
+              <!--<button class="row4-one">Work Details</button>-->
+              <div class="row4-one">
+                <popper
+                  trigger="clickToOpen"
+                  :options="{
+                    placement: 'right',
+                    modifiers: { offset: { offset: '0,10px' } }
+                  }">
+                  <div class="popper">
+                    <p>popper content</p>
+                    <p>popper content</p>
+                    <p>popper content</p>
+                  </div>
+              
+                  <button slot="reference">
+                    Work Details
+                  </button>
+                </popper>                 
+              </div>                        
+
+            </div>
           </div>          
         </div>
       </div>
@@ -69,8 +104,13 @@
 
 <script>
 //import Vue from 'vue';
+import Popper from 'vue-popperjs';
+import 'vue-popperjs/dist/vue-popper.css';
 
 export default {
+  components: {
+    'popper': Popper
+  },  
   data() {
     return {
       wid: this.$cookies.get("id"),
@@ -87,7 +127,10 @@ export default {
       .catch((error) => {
         alert(error);
       })     
-    },   
+    },
+    fnn() {
+
+    }   
   },
   created() {
    this.apiCall()
@@ -98,7 +141,8 @@ export default {
 <style scoped>
   .mw-body {
     padding-top: 10px;
-    padding-left: 30px;
+    padding-left: 50px;
+    height: 1500px;
   }
   .ty-header {
     font-weight: bold;
@@ -107,18 +151,19 @@ export default {
     color: #000;    
   }
   .box-wrapper {
-    width: 80%;
+    width: 85%;
     margin-top: 10px;
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
   } 
   .box {
     width: 300px;
-    height: 200px;
+    height: 230px;
     display: grid;
     grid-template-rows: 30% 25% 30% 15%;
     background-color: #fff;
-    border: 1px solid #F5F5F5;    
+    border: 1px solid #F5F5F5; 
+    margin-bottom: 30px;
   } 
   .sh-all {
     box-shadow: 0 0 5px #DBDBDB;
@@ -152,7 +197,7 @@ export default {
   }
   .box-row3 {
     text-align: center;
-    margin-top: 0px;
+    margin-top: 15px;
   }
   .row3-one {
     font-size: 40px;
@@ -164,11 +209,18 @@ export default {
   .row4-one {
     font-size: 14px;
   }
-
   .yd-header {
     font-weight: bold;
-    margin-top: 50px;
+    margin-top: 30px;
     font-size: 20px;
     color: #000;    
+  }
+  .popper {
+    background-color: #000;
+    color: #fff;
+    width: 200px;
+    height: 200px;
+    padding: 10px;
+    border: none;
   }
 </style>
