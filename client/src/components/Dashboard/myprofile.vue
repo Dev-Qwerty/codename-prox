5cd8a123a97ffb309ff421f5dc2a706374d1cad9
alanmatw <alanmatw@gmail.com>
2020-06-30 16:40:21 +0530

    feat(myprofile): add box contents

--- client/src/components/Dashboard/myprofile.vue
+++ client/src/components/Dashboard/myprofile.vue
@@ -1,10 +1,39 @@
 <template>
   <div class="mp-wrapper">
     <div class="mp-box">
-      <div class="top-hdn">
+      <div class="box-top-section">
         <p>My Profile</p>  
-      </div>      
-
+      </div>   
+      <div class="box-body">
+        <div class="flex-wrapper">
+          <p>Name:</p>
+          <p class="item2">{{ reqarr.name }}</p>
+        </div>
+        <div class="flex-wrapper">
+          <p>Email:</p>
+          <p class="item2">{{ reqarr.email }}</p>
+        </div>
+        <div class="flex-wrapper">
+          <p>Phone No:</p>
+          <p class="item2">{{ reqarr.phoneNo }}</p>
+        </div>
+        <div class="flex-wrapper">
+          <p>Service:</p>
+          <p class="item2">{{ reqarr.service }}</p>
+        </div>       
+        <div class="flex-wrapper">
+          <p>Worker Type:</p>
+          <p class="item2">{{ reqarr.workerType }}</p>
+        </div>   
+        <div class="flex-wrapper">
+          <p>Rating:</p>
+          <p class="item2">{{ reqarr.rating }}</p>
+        </div>
+        <div class="flex-wrapper">
+          <p>Total Number of Works:</p>
+          <p class="item2">{{ reqarr.totalWorks }}</p>
+        </div>                                          
+      </div>   
     </div>
   </div>    
 </template>
@@ -26,7 +55,7 @@ export default {
       this.$http.get(url)
       .then((response) => {
         this.reqarr = response.data
-        alert(JSON.stringify(this.reqarr))
+        //alert(JSON.stringify(this.reqarr))
       })
       .catch((error) => {
         alert(error);
@@ -43,11 +72,11 @@ export default {
   <style scoped>
   .mp-wrapper {
     width: 100%;
-    height: 1000px
+    height: 700px
   }
   .mp-box {
     /*box-shadow: -5px 5px 5px #DBDBDB;*/
-    box-shadow: 0 0 10px #DBDBDB;
+    box-shadow: 0 0 8px #DBDBDB;
     width: 60%;
     height: 500px;
     background-color: #fff;
@@ -55,14 +84,24 @@ export default {
     margin-right: auto;
     margin-top: 80px;
   }
-  .top-hdn {
+  .box-top-section {
     font-size: 15px;
-    padding-left: 10px;
-    padding-top: 5px;
-    padding-bottom: 5px;
-    height: 36px;
+    padding-left: 15px;
+    padding-top: 10px;
+    height: 40px;
     width: 100%;
-    color: #aaa;
+    color: #fff;
     background-color: #000;
+  }
+  .box-body {
+    margin-left: 35px;
+    margin-top: 35px;
+    color: #000;
+  }
+  .flex-wrapper {
+    display: flex;
+  }
+  .item2 {
+    margin-left: 10px;
   }
 </style>
