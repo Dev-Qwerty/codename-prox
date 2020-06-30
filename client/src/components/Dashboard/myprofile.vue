f02b60d6f4b152691e056e98c0531517c764a98d
alanmatw <alanmatw@gmail.com>
2020-06-30 11:03:49 +0530

    feat(worker-dashboard): create myprofile component initial structure

---
+++ client/src/components/Dashboard/myprofile.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wr-wrapper">
+    <div class="wr-grid">
+
+
+    </div>
+  </div>    
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default {
+  data() {
+    return {
+      wid: this.$cookies.get("id"),
+      reqarr: []
+    }
+  },
+  methods: {
+    apiCall() {
+      //let url = 'http://localhost:3000/worker/getCompleteProfile/f35ce2de4348f6943f9621bed9af307f'
+      let url = 'http://localhost:3000/worker/getCompleteProfile/' + this.wid
+      this.$http.get(url)
+      .then((response) => {
+        this.reqarr = response.data
+        alert(this.reqarr)
+      })
+      .catch((error) => {
+        alert(error);
+      })     
+    },
+ 
+  },
+  created() {
+   this.apiCall()
+  }
+} 
+</script>
+
+  <style scoped>
+  .wr-grid {
+    width: 100%;
+    height: 1500px;
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+  }
+
+</style>
