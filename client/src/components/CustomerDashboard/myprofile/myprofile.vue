a8db4c2d83f4edbf49c885fc4a1db2bf8f8d0ba0
alanmatw <alanmatw@gmail.com>
2020-07-12 22:18:48 +0530

    feat(customer-dashboard): create initial structure - bookings

---
+++ client/src/components/CustomerDashboard/myprofile/myprofile.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="Wrapper">
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  methods: { 
+    fn() {
+    }
+  }   
+}       
+</script>
+
+<style scoped>
+  .Wrapper {
+
+  }
+</style>
